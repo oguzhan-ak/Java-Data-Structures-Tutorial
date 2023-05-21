f9ccc4cad9978289da963b128186075a5d443afc
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2023-05-21 12:33:46 +0300

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        String slash = "/////////////////////////////////";
+        
         // Arrays
-        String a = "sdf";
+        String[] colors = new String[5];
+        colors[0] = "asd";
+        System.out.println(Arrays.toString(colors));
+        System.out.println(colors[2]);
+        System.out.println(slash);
+        int[] numbers = {100, 200};
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+        System.out.println(slash);
+        Arrays.stream(colors).forEach(System.out::println);
+        System.out.println(slash);
     }
 }
