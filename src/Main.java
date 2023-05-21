62a3fafef9308da2b9e8fd38007b481b58ed31db
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2023-05-21 12:41:07 +0300

    2D Arrays

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,27 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         String slash = "/////////////////////////////////";
-        
-        // Arrays
-        String[] colors = new String[5];
-        colors[0] = "asd";
-        System.out.println(Arrays.toString(colors));
-        System.out.println(colors[2]);
-        System.out.println(slash);
-        int[] numbers = {100, 200};
-        for (int number : numbers) {
-            System.out.println(number);
+
+        // 2D Arrays
+        char[][] board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
         }
+
+        System.out.println(Arrays.deepToString(board));
         System.out.println(slash);
-        Arrays.stream(colors).forEach(System.out::println);
+
+        board[0][0] = 'O';
+        board[1][0] = 'O';
+        board[2][0] = 'O';
+
+        System.out.println(Arrays.deepToString(board));
         System.out.println(slash);
+
+        char[][] board2 = {new char[]{'0', '-', '-'}, new char[]{'0', '-', '-'}, new char[]{'0', '-', '-'}};
+
+        System.out.println(Arrays.deepToString(board2));
     }
 }
