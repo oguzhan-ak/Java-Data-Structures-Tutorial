48917d326861beb0f59a9c69531a5c141ff43f32
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2023-05-24 21:56:12 +0300

    LinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,30 @@
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
         String slash = "/////////////////////////////////";
 
-        // Queue
-        Queue<Person> superMarket = new LinkedList<>();
-        superMarket.add(new Person("Oğuzhan", 23));
-        superMarket.add(new Person("Ahmet", 32));
-        superMarket.add(new Person("Ayşe", 56));
+        // LinkedList
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Oğuzhan", 45));
+        linkedList.add(new Person("Mehmet", 35));
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+        while (personListIterator.hasNext()) {
+            System.out.println(personListIterator.next());
+        }
+    }
 
-        System.out.println(superMarket.size());
-        System.out.println(superMarket.peek());
-        System.out.println(superMarket.poll());
-        System.out.println(superMarket.size());
-        System.out.println(superMarket.peek());
+    private static void queues() {
+        LinkedList<Person> queue = new LinkedList<>();
+        queue.add(new Person("Oğuzhan", 23));
+        queue.add(new Person("Ahmet", 32));
+        queue.add(new Person("Ayşe", 56));
+        System.out.println(queue.size());
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue.size());
+        System.out.println(queue.peek());
     }
 
     record Person(String name, int age) {
