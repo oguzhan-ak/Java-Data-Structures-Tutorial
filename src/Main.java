2d6752e0892cc40d597dfb436c578ae33c4c660b
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2023-05-24 21:33:42 +0300

    Stack

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,13 @@ public class Main {
         stack.push(1);
         stack.push(2);
         stack.push(3);
-//        System.out.println(stack.pop());
-//        System.out.println(stack.size());
-//        System.out.println(stack.pop());
-//        System.out.println(stack.size());
-//        System.out.println(stack.pop());
-//        System.out.println(stack.size());
-//        System.out.println(stack.empty());
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+        System.out.println(stack.empty());
         System.out.println(stack.search(2));
     }
 }
