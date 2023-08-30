511989c089f09ff67cb27b3e337838aeb94aa5d9
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2023-08-30 21:25:43 +0300

    hashCode

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,19 @@ public class Main {
         
     }
 
+    private static void hashcode() {
+        Map<Person, Diamond> map = new HashMap<>();
+        map.put(new Person("Cemile"), new Diamond("African Diamond"));
+        System.out.println(map.get(new Person("Cemile")));
+        System.out.println(new Person("Cemile").hashCode());
+        System.out.println(new Person("Cemile").hashCode());
+    }
+
     private static void queues() {
         LinkedList<Person> queue = new LinkedList<>();
-        queue.add(new Person("Oğuzhan", 23));
-        queue.add(new Person("Ahmet", 32));
-        queue.add(new Person("Ayşe", 56));
+        queue.add(new Person("Oğuzhan"));
+        queue.add(new Person("Ahmet"));
+        queue.add(new Person("Ayşe"));
         System.out.println(queue.size());
         System.out.println(queue.peek());
         System.out.println(queue.poll());
@@ -31,10 +39,10 @@ public class Main {
 
     private static void maps() {
         Map<Integer, Person> personMap = new HashMap<>();
-        personMap.put(1, new Person("alex", 23));
-        personMap.put(2, new Person("morgan", 22));
-        personMap.put(3, new Person("ozzy", 22));
-        personMap.put(3, new Person("ozzyy", 22));
+        personMap.put(1, new Person("alex"));
+        personMap.put(2, new Person("morgan"));
+        personMap.put(3, new Person("ozzy"));
+        personMap.put(3, new Person("ozzyy"));
         System.out.println(personMap);
         System.out.println(personMap.size());
         System.out.println(personMap.get(2));
@@ -46,25 +54,52 @@ public class Main {
         personMap.forEach((key, person) -> System.out.println(key + " - " + person));
         personMap.remove(3);
         personMap.forEach((key, person) -> System.out.println(key + " - " + person));
-        System.out.println(personMap.getOrDefault(4, new Person("default", 10)));
+        System.out.println(personMap.getOrDefault(4, new Person("default")));
     }
 
     private static void linkedLists() {
         LinkedList<Person> linkedList = new LinkedList<>();
-        linkedList.add(new Person("Oğuzhan", 45));
-        linkedList.add(new Person("Mehmet", 35));
+        linkedList.add(new Person("Oğuzhan"));
+        linkedList.add(new Person("Mehmet"));
         ListIterator<Person> personListIterator = linkedList.listIterator();
         while (personListIterator.hasNext()) {
             System.out.println(personListIterator.next());
         }
     }
 
-
-    record Person(String name, int age) {
+    record Diamond(String name) {
 
     }
 
     record Ball(String color) {
 
+    }
+
+    static class Person {
+        String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
     }
 }
